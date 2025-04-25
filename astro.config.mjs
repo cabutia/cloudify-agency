a60700d1da6a7241b21d9cd512235c43f900f0a8
Leandro Gomez <leandro.grotnig@gmail.com>
2025-04-25 16:23:31 -0300

    feat: initialize base everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+import tailwindcss from "@tailwindcss/vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        resolve: {
+            alias: {
+                "@": resolve(__dirname, "src"),
+            },
+        },
+
+        plugins: [tailwindcss()],
+    },
+});
---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_NAME = "TBD"; // To be defined
+export const SITE_URL = "https://tbd.com"; // To be defined
